8bf036a6fda2ee07d01c60c7643e8ff0b146a900
Zsofia <zsofia.helmeczi@gmail.com>
2017-06-08 17:49:29 +0200

    Locating & Installing Angular Modules - HttpModule

--- src/vendor.ts
+++ src/vendor.ts
@@ -3,6 +3,8 @@ import '@angular/core';
 import '@angular/common';
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
+import '@angular/http';
+
 
 // 3rd party
 import 'core-js';
